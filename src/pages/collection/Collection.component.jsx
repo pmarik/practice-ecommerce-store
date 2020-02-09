66d7bb09e568e954862b6a83aefb971e4f799e45
pmarik <pmarik@uwalumni.com>
2020-02-09 15:34:55 -0600

    updated selectors and components with new directory flow for redux

--- src/pages/collection/Collection.component.jsx
+++ src/pages/collection/Collection.component.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './collection.styles.scss';
+import { connect } from 'react-redux';
+import { selectCollection } from '../../redux/shop/shop.selectors.js';
 
 import CollectionItem from '../../components/collection-item/CollectionItem.component';
 
-const CollectionPage = () => (
-    <div className="collection-page">
-        <h2>Collection page</h2>
-    </div>
-)
+const CollectionPage = ({ collection }) => {
 
-export default CollectionPage;
+    const { title, items } = collection;
+
+    return (
+    <div className="collection-page">
+        <h2 className="title">{ title }</h2>
+        <div className="items">
+            { items.map(item => (<CollectionItem key={item.id} item={item} />))}
+        </div>
+    </div>
+    )
+}
+
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);  
