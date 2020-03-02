4cdc81d9b05af4340b1c7d59f4c09e86d52406fd
pmarik <pmarik@uwalumni.com>
2020-03-02 17:50:35 -0600

    mobile styles on home page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/Homepage.component';
 import ShopPage from './pages/shop/ShopPage.component';
 import CheckoutPage from './pages/checkout/Checkout.component';
 import SignInandSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import './App.css';
+import { GlobalStyle } from './global.styles';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -37,13 +37,14 @@ const App = ({ setCurrentUser, currentUser }) => {
 
     return (
       <div>
+        <GlobalStyle />
         <Header/> 
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path="/signin" render={() => currentUser ? (<Redirect to="/" />) : (<SignInandSignUpPage />) } /> 
-        </Switch>  
+        </Switch> 
       </div>
     );
  
